598a012286e6a28643dcc8201647614c4dfe9e86
moinbukhari <moinbukhari4321@gmail.com>
2023-10-21 19:35:01 +0100

    basic front page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,62 @@
-import Image from 'next/image'
-import {Button} from '../components/ui/button'
+"use client";
+
+import Image from "next/image";
+import { Button } from "../components/ui/button";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="outline" size="lg">Hello</Button>
+    <main className="flex flex-col  min-h-screen bg-slate-100">
+      <div className="flex flex-col w-full items-center py-5">
+        <h2 className="text-3xl font-semibold text-blue-700">Find Your MP</h2>
+      </div>
+      <div className="px-4">
+        <Command>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+          </CommandList>
+        </Command>
+      </div>
+      <div className="px-4 py-2">
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
     </main>
-  )
+  );
 }
