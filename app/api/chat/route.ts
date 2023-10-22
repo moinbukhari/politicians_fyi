82d3e7a66d135aa3524d8767b806b4340da3ad59
Aman Ibrahim <amanmibra@gmail.com>
2023-10-22 17:41:35 -0400

    move prompt --> chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,23 +1,31 @@
-import OpenAI from "openai";
+import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
+import genPromt, { type PromptOptions } from "../utils/genPrompt";
+
 // Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
+const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(config);
 
 // IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+  const { options } = await req.json();
 
   // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
+  const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     stream: true,
-    messages,
+    messages: [
+      {
+        role: "user",
+        content: genPromt(options),
+      },
+    ],
   });
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
