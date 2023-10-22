9a3e4638a40c394d331e4d2e6c9668eecc3fc856
Aman Ibrahim <amanmibra@gmail.com>
2023-10-22 17:41:22 -0400

    Added completion route

---
+++ app/api/completion/route.ts
@@ -0,0 +1,30 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { prompt } = await req.json();
+
+  console.log(prompt);
+
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.completions.create({
+    model: "text-davinci-003",
+    stream: true,
+    prompt,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
