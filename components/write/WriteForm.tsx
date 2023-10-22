90212574a580536ea24d7daf13ba7de85d5ab2e4
Aman Ibrahim <amanmibra@gmail.com>
2023-10-22 19:34:49 -0400

    Get responses working

--- components/write/WriteForm.tsx
+++ components/write/WriteForm.tsx
@@ -7,6 +7,7 @@ import { useChat, useCompletion } from "ai/react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
 import { Textarea } from "../ui/textarea";
 
 // utils
@@ -18,15 +19,32 @@ const WriteForm = () => {
   const [additionalInfo, setAdditionalInfo] = useState("");
   const [prompt, setPrompt] = useState("");
 
-  console.log("test", genPromt({ name, address, additionalInfo }));
+  // setup chat
+  const { setInput, handleSubmit, isLoading, messages } = useChat({
+    body: {
+      options: {
+        name,
+        address,
+        additionalInfo,
+      },
+      prompt,
+    },
+  });
 
-  const onSubmit = () => {
-    setPrompt(genPromt({ name, address, additionalInfo }));
+  const lastMessage = messages[messages.length - 1];
+  const generatedEmail =
+    lastMessage?.role === "assistant" ? lastMessage.content : null;
+
+  console.log(lastMessage);
+
+  const onSubmit = (e: any) => {
+    setInput(genPromt({ name, address, additionalInfo }));
+    handleSubmit(e);
   };
 
   return (
-    <div className="grid gap-2">
-      <div>
+    <div>
+      <form className="grid gap-4 mb-6" onSubmit={onSubmit}>
         <Label htmlFor="name">Name</Label>
         <Input
           id="name"
@@ -34,8 +52,6 @@ const WriteForm = () => {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-      </div>
-      <div>
         <Label htmlFor="address">Address</Label>
         <Textarea
           id="address"
@@ -43,8 +59,6 @@ const WriteForm = () => {
           value={address}
           onChange={(e) => setAddress(e.target.value)}
         />
-      </div>
-      <div>
         <Label htmlFor="additionalInfo">
           Add any additional information, such as your occupation, about
           yourself that you would like the ✨AI✨ to know in order to
@@ -56,9 +70,45 @@ const WriteForm = () => {
           value={additionalInfo}
           onChange={(e) => setAdditionalInfo(e.target.value)}
         />
-      </div>
 
-      <Button onClick={onSubmit}>✨ Generate Personalized Email ✨</Button>
+        {!isLoading && (
+          <Button type="submit">✨ Generate Personalized Email ✨</Button>
+        )}
+        {isLoading && <Button disabled> Generating...</Button>}
+      </form>
+      <Separator />
+      <output className="space-y-10 my-10">
+        {generatedEmail && (
+          <>
+            <div>
+              <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
+                Your generated email
+              </h2>
+            </div>
+            <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+              {generatedEmail
+                .substring(generatedEmail.indexOf("1") + 3)
+                .split("2.")
+                .map((email) => {
+                  return (
+                    <div
+                      className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                      onClick={() => {
+                        navigator.clipboard.writeText(email);
+                        // toast("Bio copied to clipboard", {
+                        //   icon: "✂️",
+                        // });
+                      }}
+                      key={email}
+                    >
+                      <p>{email}</p>
+                    </div>
+                  );
+                })}
+            </div>
+          </>
+        )}
+      </output>
     </div>
   );
 };
