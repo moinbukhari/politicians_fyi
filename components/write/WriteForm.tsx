bf90bea1d175bc3d111ed51836097c70ae317499
Aman Ibrahim <amanmibra@gmail.com>
2023-10-22 14:56:19 -0400

    Add default chat to UI

--- components/write/WriteForm.tsx
+++ components/write/WriteForm.tsx
@@ -1,7 +1,38 @@
 "use client";
 
+import { useChat } from "ai/react";
+
 const WriteForm = () => {
-  return <div>WriteForm</div>;
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/prompt",
+  });
+
+  return (
+    <div>
+      <section className="mb-auto m">
+        {messages.map((m) => (
+          <div className="mb-4" key={m.id}>
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </div>
+        ))}
+      </section>
+      <form className="flex space-x-4" onSubmit={handleSubmit}>
+        <input
+          className="rounded-md p-2 text-black"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Say something..."
+        />
+        <button
+          className="border-solid border-2 border-white p-2 rounded-md"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default WriteForm;
